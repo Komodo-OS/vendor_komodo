rio-31/android_vendor_exui

vendor: Import custom Screenshot service permissions
Change-Id: I9ef7f1d170440d877795164482624a7d2f8c8e79
Signed-off-by: Lucchetto <lucchetto.tie@live.com>

config/common.mk
@@ -148,4 +148,8 @@ include vendor/pixelstyle/config.mk
 # Customization
 include vendor/google/customization/config.mk
 
+# Screenshot permissions
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/com.android.screenshot.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.android.screenshot.xml
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

Komodo-OS/vendor_komodo

Signed-off-by: goodmeow <harunbam3@gmail.com>

config/common.mk
@@ -188,4 +188,8 @@ endif
 # Pixel Style
 include vendor/pixelstyle/config.mk
 
+# Screenshot permissions
+PRODUCT_COPY_FILES += \
+    vendor/komodo/config/permissions/com.android.screenshot.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.android.screenshot.xml
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk