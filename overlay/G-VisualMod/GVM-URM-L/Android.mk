17530fcd3a882fc190ac3bf749fc469739ca5d35
goodmeow <harunbam3@gmail.com>
2020-06-22 00:52:29 +0700

    overlay: overhaul 2/4: gvisualmod: relocate and add licencing - increase priority to 100

---
+++ overlay/G-VisualMod/GVM-URM-L/Android.mk
@@ -0,0 +1,28 @@
+# Copyright (C) 2020 Komodo-OS-Rom
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_RRO_THEME := GVM-URM-L
+LOCAL_PACKAGE_NAME := GVM-URM-L
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRODUCT_MODULE := true
+LOCAL_PRIVILEGED_MODULE := false
+
+include $(BUILD_RRO_PACKAGE)
