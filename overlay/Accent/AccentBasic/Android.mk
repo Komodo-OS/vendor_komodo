f085e0802500500eba59c179dcd3205230e1c915
goodmeow <harunbam3@gmail.com>
2020-06-22 01:48:08 +0700

    overlay: accent: cleanup AccentUserCreated

--- overlay/Accent/AccentBasic/Android.mk
+++ overlay/Accent/AccentBasic/Android.mk
@@ -1,1 +1,30 @@
-include $(call all-subdir-makefiles)
+LOCAL_PATH := vendor/komodo/overlay/Accent/AccentBasic
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := AccentBasic
+LOCAL_REQUIRED_MODULES := \
+	  amber \
+	  black \
+	  blue \
+	  blueGrey \
+	  brown \
+	  cyan \
+	  deepOrange \
+	  deepPurple \
+	  green \
+	  indigo \
+	  lightBlue \
+	  lightGreen \
+	  lime \
+	  orange \
+	  pink \
+	  purple \
+	  red \
+	  teal \
+	  white \
+	  yellow
+
+include $(BUILD_PHONY_PACKAGE)
+
+include $(call first-makefiles-under,$(LOCAL_PATH))
