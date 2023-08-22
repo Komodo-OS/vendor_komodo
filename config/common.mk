SuperiorOS/android_vendor_superior

sysconfig: Import pixel_2016_exclusive XML
- Let vanilla users enjoy unlimited Google Photos

Change-Id: I3cdb84f3e8d148b2619b5490bf18b744c5b417d4

config/common.mk
@@ -78,6 +78,10 @@ ifeq ($(SUPERIOR_GAPPS), full)
      $(call inherit-product, vendor/gms/products/gms.mk)
 endif
 
+# Google Photos Pixel Exclusive XML
+PRODUCT_COPY_FILES += \
+    vendor/superior/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Superior Permissions
 PRODUCT_COPY_FILES += \
     vendor/superior/config/permissions/privapp-permissions-superior-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-superior.xml \

Komodo-OS/vendor_komodo

Signed-off-by: nadinsylaa <asepmlnaaa@gmail.com>

config/common.mk
@@ -72,6 +72,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
+# Google Photos Pixel Exclusive XML
+PRODUCT_COPY_FILES += \
+    vendor/komodo/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Power whitelist
 PRODUCT_COPY_FILES += \
     vendor/komodo/config/permissions/custom-power-whitelist.xml:system/etc/sysconfig/custom-power-whitelist.xml