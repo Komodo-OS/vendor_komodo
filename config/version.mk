86761fb41f7a095c135eb5db28459b5cd148e76d
wulan17 <wulan17@nusantararom.org>
2025-02-26 19:33:12 +0700

    vendor: Update versioning
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- config/version.mk
+++ config/version.mk
@@ -7,7 +7,15 @@
 
 KOMODO_BRAND := KomodoOS
 KOMODO_BUILD_VERSION := Varanus
-KOMODO_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
+KOMODO_BASE_VERSION := 5.0
+
+KOMODO_DATE_YEAR := $(shell date -u +%Y)
+KOMODO_DATE_MONTH := $(shell date -u +%m)
+KOMODO_DATE_DAY := $(shell date -u +%d)
+KOMODO_DATE_HOUR := $(shell date -u +%H)
+KOMODO_DATE_MINUTE := $(shell date -u +%M)
+KOMODO_BUILD_DATE_UTC := $(shell date -d '$(KOMODO_DATE_YEAR)-$(KOMODO_DATE_MONTH)-$(KOMODO_DATE_DAY) $(KOMODO_DATE_HOUR):$(KOMODO_DATE_MINUTE) UTC' +%s)
+KOMODO_BUILD_DATE := $(KOMODO_DATE_YEAR)$(KOMODO_DATE_MONTH)$(KOMODO_DATE_DAY)-$(KOMODO_DATE_HOUR)$(KOMODO_DATE_MINUTE)
 
 DEVICE_LIST := $(shell cat official_devices/devices.list)
 
@@ -16,11 +24,16 @@ ifneq (,$(findstring $(KOMODO_BUILD),$(DEVICE_LIST)))
 else
     KOMODO_BUILD_TYPE := UNOFFICIAL
 endif
-KOMODO_VERSION := $(KOMODO_BRAND)_$(KOMODO_BUILD)-$(KOMODO_BUILD_VERSION)-$(KOMODO_BUILD_DATE)-$(KOMODO_BUILD_TYPE)
+
+KOMODO_VERSION := $(KOMODO_BRAND)-v$(KOMODO_BASE_VERSION)-$(KOMODO_BUILD)-$(KOMODO_BUILD_TYPE)-$(KOMODO_BUILD_VERSION)-$(KOMODO_BUILD_DATE)
+
 # KomodoOS Platform Version
 PRODUCT_PRODUCT_PROPERTIES += \
-    org.komodo.build.version=$(KOMODO_BUILD_VERSION) \
-    org.komodo.build.date=$(KOMODO_BUILD_DATE) \
-    org.komodo.build.type=$(KOMODO_BUILD_TYPE) \
-    org.komodo.version=$(KOMODO_VERSION)
+    org.komodo.version=$(KOMODO_BASE_VERSION) \
+    org.komodo.version.name=$(KOMODO_BUILD_VERSION) \
+    org.komodo.version.display=$(KOMODO_VERSION) \
+    org.komodo.build_date=$(KOMODO_BUILD_DATE) \
+    org.komodo.build_date_utc=$(KOMODO_BUILD_DATE_UTC) \
+    org.komodo.build_type=$(KOMODO_BUILD_TYPE) \
+    ro.komodo.maintainer=$(Komodo_MAINTAINER)
 
