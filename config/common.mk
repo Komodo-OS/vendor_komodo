b6be0456f7ee558addf6ec3a22add7d546da1256
Kizziama <kizziama@proton.me>
2025-03-14 04:12:04 +0000

    vendor: Build GameSpace
    Change-Id: Ic540925d58e298b6157e27c205ee335b8b526c9f
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- config/common.mk
+++ config/common.mk
@@ -171,6 +171,10 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/%/libfuse-lite.so \
     system/%/libntfs-3g.so
 
+# GameSpace
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # GAPPS
 ifeq ($(WITH_GAPPS), true)
     $(call inherit-product, vendor/google/gms/config.mk)
